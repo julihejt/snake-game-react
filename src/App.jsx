0a5819c6c01bb3202d925770df782ed5cffbaeb2
julihejt <juliehejtmanek00@gmail.com>
2025-04-08 18:51:33 +0200

    react-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
